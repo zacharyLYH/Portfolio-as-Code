0e3c6bb25d7ce796237ccc10985e43ce1d87aed4
Zachary Lee <leeyihong03@gmail.com>
2024-10-01 14:02:17 +0800

    enhance footer ui

--- page/footer.tsx
+++ page/footer.tsx
@@ -1,22 +1,26 @@
-import { GitHubLogoIcon } from "@radix-ui/react-icons";
+import { GitHubLogoIcon } from '@radix-ui/react-icons';
+import Link from 'next/link';
 
-export default function Footer(){
+export default function Footer() {
     return (
-        <footer className="bg-gray-100 rounded-lg shadow mt-8">
-        <div className="flex flex-col w-full mx-auto max-w-screen-xl p-4 gap-y-2 items-center ">
-            <span className="flex items-center justify-center space-x-2">
-                <GitHubLogoIcon className="w-6 h-6" />
-                <a
-                    href="https://github.com/zacharyLYH/portfolio-as-code"
-                    className="hover:underline text-blue-500"
-                >
-                    Source code
+        <footer className="rounded-lg shadow mt-8">
+            <div className="flex flex-row justify-between w-full mx-auto max-w-screen-xl p-4 gap-y-2 items-center">
+                <span className="flex items-center justify-center space-x-2">
+                    <GitHubLogoIcon className="w-6 h-6" />
+                    <a
+                        href="https://github.com/zacharyLYH/portfolio-as-code"
+                        className="hover:underline text-blue-500 text-sm"
+                    >
+                        Source code
+                    </a>
+                </span>
+                <a href="https://zacharyLYH.bio" className="hover:underline text-blue-500 text-sm">
+                    Created with 🫶 by Zachary Lee
                 </a>
-            </span>
-            <a href="https://zacharyLYH.bio" className="hover:underline text-blue-500 text-sm">
-                Created with 🫶 by Zachary Lee
-            </a>
-        </div>
-    </footer>
-    )
+                <Link href="/configure" className="hover:underline text-blue-500 text-sm">
+                    ✔️ Build your own portfolio now
+                </Link>
+            </div>
+        </footer>
+    );
 }
